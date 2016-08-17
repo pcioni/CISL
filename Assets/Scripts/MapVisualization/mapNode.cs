7b750bda7d3e99c07f4341a606f78fd99256c6ee
Craig Carlson <cjcarlson20@gmail.com>
2016-08-17 16:09:58 -0400

    minor fix

--- Assets/Scripts/MapVisualization/mapNode.cs
+++ Assets/Scripts/MapVisualization/mapNode.cs
@@ -18,6 +18,7 @@ public class mapNode : MonoBehaviour {
 		lr = GetComponent<LineRenderer>();
 		img = GetComponent<Image>();
 		master.callback = OnStateChange;
+		name = "mapNode(" + master.name + ")";
 	}
 	
 	public void OnStateChange(string state) {
@@ -31,15 +32,18 @@ public class mapNode : MonoBehaviour {
 				img.color = tmp;
 				break;
 			case "OUT":
+				lr.enabled = false;
 				transform.SetAsFirstSibling();
 				tmp.a = 0;
 				img.color = tmp;
 				break;
 			case "HALF":
+				lr.enabled = false;
 				transform.SetAsFirstSibling();
 				img.color = tmp;
 				break;
 			case "PAST":
+				lr.enabled = false;
 				img.color = tmp;
 				break;
 			case "RE":
@@ -48,6 +52,7 @@ public class mapNode : MonoBehaviour {
 				img.color = tmp;
 				break;
 			case "BACK":
+				lr.enabled = false;
 				img.color = tmp;
 				break;
 		}
@@ -55,6 +60,7 @@ public class mapNode : MonoBehaviour {
 
 	private IEnumerator tmplcr = null;
 	public void drawLines() {
+		lr.enabled = true;
 		if (tmplcr != null) {
 			StopCoroutine(tmplcr);
 		}
@@ -65,12 +71,14 @@ public class mapNode : MonoBehaviour {
 	private IEnumerator _drawLines() {
 		//yield return new WaitForSeconds(1);
 		Vector3 centralNodePos = transform.position;
+		centralNodePos.z = 1;
 		Vector3[] points = new Vector3[Mathf.Max(neighbors.Count * 2, 1)];
 		points[0] = centralNodePos;
 		int nCount = 0;
 		for (int i = 1; i < points.Length; i += 2) {
 			points[i - 1] = centralNodePos;
 			points[i] = neighbors[nCount].transform.position;
+			points[i].z = 1;
 			nCount++;
 			yield return null;
 		}
