a03ebf877c7e06fedabbc45cbf75a4496532c543
david allen <mail@ludimotion.com>
2016-09-17 10:26:57 -0400

    Screenshot script.
    Attach this to any object in the game & press "SHIFT + C + S" to capture screen.
    Files are saved in "Screenshots" directory by default, but you can change the property in the component editor.
    Default resolution is 1.0f (72 dpi), change it to 4.0 for print resolution.

---
+++ Assets/Scripts/Utils/SaveScreenshot.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.IO;
+using System;
+
+public class SaveScreenshot : MonoBehaviour {
+
+	// TODO: create radio list for resolution presets: 
+	//        - "Web" 1.0f
+	//        - "Print" 4.0f //actually 4.17, but 4.0f is cleaner
+	//        - "Custom" // have it enable the "resolution" entry field which should otherwise be greyed out/disappear
+	// TODO: allow file browsing for specification of directory
+	// TODO: allow for camera picking (to screenshot one camera only, as opposed to entire scene with three setups)
+
+	public float resolution = 1.0f;
+	public string screenshotDirectory = "Screenshots";
+
+	private bool saved = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		// capture screenshot when SHIFT + C + S are pressed
+		bool shiftDown = (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift));
+		bool allDown = (shiftDown && Input.GetKey (KeyCode.S) && Input.GetKey (KeyCode.C));
+
+		if (allDown && !saved) {
+			// prepare PNG filename using date, time, and scene name
+			DateTime dtUTC = System.DateTime.UtcNow;
+			string fext = ".png";
+			string datePatt = @"yyyyMMdd_hhmmss";
+			string fname = dtUTC.ToString (datePatt) + "_" + SceneManager.GetActiveScene ().name + fext;
+
+			// append filename to screenshot dir name
+			string fpath = Path.Combine (screenshotDirectory, fname);
+
+			// do the thing
+			Application.CaptureScreenshot (fpath);
+			saved = true; // to avoid saving repeatedly while keys are down
+
+			// debug
+			Debug.Log ("SaveScreenshot :: fpath = " + fpath);
+
+		} else {
+			saved = false;
+		}
+	}
+}
