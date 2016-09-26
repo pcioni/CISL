ca99d9c55934cecb16c3a10da75f61a2973f47c2
phelow <wpheloung@gmail.com>
2016-09-26 15:25:00 -0400

    Camera momentum added

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,29 +6,37 @@ public class CameraController : MonoBehaviour
 	private Vector3 last_mouse_position;
 	public float minZoom = 0f;
 	public float maxZoom = 100f;
+	public float orthographicMomentum = 0.0f;
+
+	[SerializeField]private float m_maxCameraAcceleration = 4.0f;
+	[SerializeField]private float m_maxCameraVelocity = 10.0f;
 
 
 	void Update()
 	{
+		orthographicMomentum = orthographicMomentum / 1.1f;
+
+		orthographicMomentum = orthographicMomentum < 0.05f ? 0.0f : orthographicMomentum; 
+
 		//Pan camera with mouse
-		if (Input.GetMouseButtonDown(1))
+		if (Input.GetMouseButtonDown (1))
 			last_mouse_position = Input.mousePosition;
-		if (Input.GetMouseButton(1)) {
+		if (Input.GetMouseButton (1)) {
 			Vector3 delta = last_mouse_position - Input.mousePosition;
-			Camera.main.transform.Translate(delta.x * .1f, delta.y * .1f, 0);
+			Camera.main.transform.Translate (delta.x * .1f, delta.y * .1f, 0);
 			last_mouse_position = Input.mousePosition;
-			EventManager.TriggerEvent(EventManager.EventType.INTERFACE_PAN, Camera.main.orthographicSize.ToString());
+			EventManager.TriggerEvent (EventManager.EventType.INTERFACE_PAN, Camera.main.orthographicSize.ToString ());
 		}
 
 		//scroll in
-		if (Input.GetAxis("Mouse ScrollWheel") > 0) {
-			ZoomOrthoCamera(Camera.main.ScreenToWorldPoint(Input.mousePosition), 1);
-			EventManager.TriggerEvent(EventManager.EventType.INTERFACE_ZOOM_OUT, Camera.main.orthographicSize.ToString());
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
+			ZoomOrthoCamera (Camera.main.ScreenToWorldPoint (Input.mousePosition), 1);
+			EventManager.TriggerEvent (EventManager.EventType.INTERFACE_ZOOM_OUT, Camera.main.orthographicSize.ToString ());
 		}
 		//scroll out
-		if (Input.GetAxis("Mouse ScrollWheel") < 0) {
-			ZoomOrthoCamera(Camera.main.ScreenToWorldPoint(Input.mousePosition), -1);
-			EventManager.TriggerEvent(EventManager.EventType.INTERFACE_ZOOM_IN, Camera.main.orthographicSize.ToString());
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+			ZoomOrthoCamera (Camera.main.ScreenToWorldPoint (Input.mousePosition), -1);
+			EventManager.TriggerEvent (EventManager.EventType.INTERFACE_ZOOM_IN, Camera.main.orthographicSize.ToString ());
 		}
 	}
 
@@ -36,6 +44,14 @@ public class CameraController : MonoBehaviour
 	// negative amount zooms in, positive zooms out
 	// TODO: stop camera movement when at zoom limit
 	void ZoomOrthoCamera(Vector3 zoomTowards, float amount) {
+		if (Mathf.Sign (amount) != Mathf.Sign (orthographicMomentum)) {
+			orthographicMomentum = 0.0f;
+		}
+
+		amount += orthographicMomentum;
+		orthographicMomentum += Mathf.Clamp(amount,-m_maxCameraAcceleration,m_maxCameraAcceleration);
+		orthographicMomentum = Mathf.Clamp (orthographicMomentum, -m_maxCameraVelocity, m_maxCameraVelocity);
+
 		// Calculate how much we will have to move towards the zoomTowards position
 		float multiplier = (1.0f / GetComponent<Camera>().orthographicSize * amount);
 
