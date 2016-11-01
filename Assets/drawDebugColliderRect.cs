2a972ea83c338217d1e9a84a6ed3e69ecd3497e2
david allen <mail@ludimotion.com>
2016-11-01 15:51:41 -0400

    Collider debug rects now resize to fit collider.

--- Assets/drawDebugColliderRect.cs
+++ Assets/drawDebugColliderRect.cs
@@ -4,30 +4,39 @@ using System.Collections;
 public class drawDebugColliderRect : MonoBehaviour {
 	[SerializeField]
 	GameObject panel;
-	private Rect panelRect;
-	private BoxCollider2D collider;
+	private RectTransform m_panelRTrans;
+	private BoxCollider2D m_collider;
 
 	void Awake () {
 
-		panelRect = panel.GetComponent<RectTransform> ().rect;
+		m_panelRTrans = panel.GetComponent<RectTransform>();
 
 		try {
-			collider = this.GetComponent<BoxCollider2D> ();
+			m_collider = this.GetComponent<BoxCollider2D> ();
 		} catch {
 		}
-	}
 
-	// Use this for initialization
-	void Start () {
+        refresh();
+    }
+
+    void refresh()
+    {
+        panel.SetActive(DebugMode.MS_ACTIVE);
+        if (m_collider != null && (m_panelRTrans.sizeDelta.x != m_collider.size.x || m_panelRTrans.sizeDelta.y != m_collider.size.y))
+        {
+            m_panelRTrans.sizeDelta = new Vector2 (m_collider.size.x, m_collider.size.y);
+            //panelRect.width = m_collider.size.x;
+            //panelRect.height = m_collider.size.y;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 
 	// Update is called once per frame
 	void Update () {
-		panel.SetActive (DebugMode.MS_ACTIVE);
-		if (collider != null) {
-			panelRect.width = collider.size.x;
-			panelRect.height = collider.size.y;
-		}
+        refresh();
 	}
 }
