01bdfa51a1cd9e8d3e3628438239f23049dbd5b4
phelow <wpheloung@gmail.com>
2016-12-12 11:01:41 -0500

    Temporary progress commit, slippy maps partially implemented

---
+++ Assets/DebugMaps.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugMaps : MonoBehaviour {
+    [SerializeField]
+    private GameObject m_camera;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (DebugMode.MS_ACTIVE)
+        {
+            RecursivelySetLayer(GameObject.Find("[Map]"), 14);
+            m_camera.active = false;
+        }
+        else
+        {
+            RecursivelySetLayer(GameObject.Find("[Map]"), 0);
+            m_camera.active = true;
+        }
+	}
+
+    void RecursivelySetLayer(GameObject go, int layer)
+    {
+        go.layer = layer;
+        foreach(Transform t in go.transform)
+        {
+            RecursivelySetLayer(t.gameObject, layer);
+        }
+    }
+}
---
+++ Assets/MapBehaviorWrapper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapBehaviorWrapper : MonoBehaviour {
+
+    UnitySlippyMap.Map.MapBehaviour mapBehavior = null;
+
+    [SerializeField]
+    private float m_latitude;
+    [SerializeField]
+    private float m_longitude;
+    [SerializeField]
+    private float m_zoom;
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(WaitForMapBehaviorAppearance());
+	}
+	
+    private IEnumerator WaitForMapBehaviorAppearance()
+    {
+        while (mapBehavior == null)
+        {
+            mapBehavior = GameObject.Find("[Map]").GetComponent<UnitySlippyMap.Map.MapBehaviour>();
+            yield return new WaitForEndOfFrame();
+        }
+
+        InitLocation();
+    }
+
+    private void InitLocation()
+    {
+        mapBehavior.CenterWGS84 = new double[2] { m_longitude, m_latitude };
+        Debug.Log(mapBehavior.CurrentZoom);
+        Debug.Log(mapBehavior.MinZoom);
+        Debug.Log(mapBehavior.MaxZoom);
+        Debug.Log(mapBehavior.ZoomStepLowerThreshold);
+        Debug.Log(mapBehavior.ZoomStepUpperThreshold);
+        mapBehavior.CurrentZoom = mapBehavior.MinZoom;
+        mapBehavior.Zoom(0.0f);
+    }
+}
